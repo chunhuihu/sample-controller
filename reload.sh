c03fea5e2e90fbf4b5011f8019771abd6afa0e02
Chunhui Hu <chunhuihu@microsoft.com>
2022-06-23 14:30:50 +0800

    123

--- reload.sh
+++ reload.sh
@@ -1,5 +1,6 @@
 git pull
 go build -o sample-controller .
 ./sample-controller -kubeconfig=$HOME/.kube/config
+kubectl delete -f artifacts/examples/crd-status-subresource.yaml
 kubectl apply -f artifacts/examples/crd-status-subresource.yaml
 
